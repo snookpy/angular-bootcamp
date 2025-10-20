e1b625fa0c852ca8892539f2ff6b888929488592
Nook <nookittipon@gmail.com>
2025-10-20 16:43:15 +0700

    deploy config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Angular Bootcamp',
+  tagline: 'Zero to Terminator in Angular',
   favicon: 'img/favicon.ico',
   markdown: {
     mermaid: true,
@@ -18,15 +18,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://snookpy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/angular-bootcamp/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'snookpy', // Usually your GitHub org/user name.
+  projectName: 'angular-bootcamp', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
@@ -46,8 +46,8 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -78,9 +78,9 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Angular Bootcamp',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Angular Bootcamp Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -90,9 +90,9 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        // {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/snookpy',
           label: 'GitHub',
           position: 'right',
         },
